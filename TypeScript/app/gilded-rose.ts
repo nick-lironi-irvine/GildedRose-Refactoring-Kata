5a71ee90f99c4054c7419624bfb4e56fb6ef0625
Nick Lironi-Irvine <nick.lironi-irvine@medenterprises.com>
2023-11-16 11:28:46 +1300

    Apply refactoring to update quality that also adds support for Conjured items

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,49 +21,77 @@ export class GildedRose {
 
   updateQuality(): Array<Item> {
     for (const item of this.items) {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality--;
-          }
+
+      {
+        // The amount that the SellIn of this item should change this day, positive or negative
+        let sellInDelta = -1;
+
+        // Determine how much SellIn should change for this item, given various factors
+        switch (item.name) {
+          case 'Sulfuras, Hand of Ragnaros':
+            sellInDelta = 0;
+            break;
+          default:
+            sellInDelta = -1;
         }
-      } else {
-        if (item.quality < maximumItemQuality) {
-          item.quality++;
-          if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.sellIn < 11) {
-              if (item.quality < maximumItemQuality) {
-                item.quality++;
-              }
+
+        // Apply the SellIn update
+        item.sellIn = item.sellIn + sellInDelta
+      }
+
+      const isPastSellIn = item.sellIn < 0;
+
+      {
+        // The amount that the quality of this item should change this day, positive or negative
+        let qualityDelta = -1;
+
+        // Determine how much quality should change for this item, given various factors
+        switch (item.name) {
+          case 'Sulfuras, Hand of Ragnaros':
+            qualityDelta = 0;
+            break;
+          case 'Aged Brie':
+            if (isPastSellIn) {
+              qualityDelta = 2
+            } else {
+              qualityDelta = 1
             }
-            if (item.sellIn < 6) {
-              if (item.quality < maximumItemQuality) {
-                item.quality++;
-              }
+            break;
+          case 'Conjured':
+            if (isPastSellIn) {
+              qualityDelta = -4
+            } else {
+              qualityDelta = -2
             }
-          }
+            break;
+          case 'Backstage passes to a TAFKAL80ETC concert':
+            if (item.sellIn < 0) {
+              // After the concert the tickets are worthless
+              qualityDelta = -item.quality
+            } else if (item.sellIn < 5) {
+              // They get really valuable closer to the date
+              qualityDelta = 3
+            } else if (item.sellIn < 10) {
+              // They get more valuable close to the date
+              qualityDelta = 2
+            } else {
+              // Else like normal
+              qualityDelta = 1
+            }
+            break;
+          default:
+            if (isPastSellIn) {
+              qualityDelta = -2
+            } else {
+              qualityDelta = -1
+            }
         }
+
+        // Apply the computed quality update
+        item.quality = Math.min(item.quality + qualityDelta, maximumItemQuality);
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.sellIn--;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
-                item.quality--;
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < maximumItemQuality) {
-            item.quality++;
-          }
-        }
-      }
+
+      // TODO consider returning a new Item here, not mutating the provided one
     }
 
     return this.items;
